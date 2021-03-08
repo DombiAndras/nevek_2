c8c58b3fa367815443acf93f4df10acef00534eb
DombiAndras <dombibad14@gmail.com>
2021-03-08 11:10:38 +0100

    a feladatok kész

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,8 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.text.Collator;
+import java.util.*;
 
 public class Main {
 
@@ -13,12 +13,90 @@ public class Main {
 
 
    static ArrayList<KeresztNevek> keresztn = new ArrayList<>();
+
     public static void main(String[] args) throws FileNotFoundException {
 	beolvas();
         //System.out.println(nevek);
 
-        System.out.println(keresztn.get(0).nev+" "+keresztn.get(0).gyakorisag);
+        //System.out.println(keresztn.get(0).nev+" "+keresztn.get(0).gyakorisag);
+        nevekAbcben();
+        f3();
+        aDBeDB();
+        f5();
+        f6();
+        f7();
+    }
 
+    private static void f5() {
+        int db=0;
+        for (int i:gyakorisagok){
+            db=db+i;
+        }
+        System.out.println(db+ " fiú nevet adtak.");
+
+    }
+
+    private static void f7() {
+        int szum=0;
+        for (int n:gyakorisagok) {
+            szum=szum+n;
+        }
+        System.out.println(1.0*szum/gyakorisagok.size());
+        System.out.printf("Nevek átlagos száma: %.2f\n",1.0*szum/gyakorisagok.size());
+        System.out.println("----");
+    }
+
+    private static void f6() {
+    int max=0;
+    int index =0;
+    /*
+    for (int i :gyakorisagok)
+        if (i>max ) max = i;{
+
+        }
+        System.out.println(max); */
+        for (int i = 0; i < gyakorisagok.size(); i++) {
+            if (gyakorisagok.get(i) > max){
+                max = gyakorisagok.get(i);
+            index = i;
+            }
+        }
+        //System.out.println(max+" "+index);
+        System.out.println("leggyakoribb név: "+nevek.get(index)+" "+max+" alkalommal fordult elő");
+        System.out.printf("leggyakoribb név: %s %d fordult elő\n",nevek.get(index),max);
+    }
+
+    private static void aDBeDB() {
+        int aDB=0;
+        int eDB=0;
+        for (String n:nevek) {
+            if (n.charAt(0) == 'A') aDB++;
+            if (n.charAt(0) == 'E') eDB++;
+        }
+        System.out.println("A betűs név:"+aDB);
+        System.out.println("E betűs név:"+eDB);
+    }
+
+    private static void f3() {
+        System.out.println("Anyakönyvezhető férfi nevek száma: "+nevek.size());
+    }
+
+    private static void nevekAbcben() {
+        ArrayList<String> ujNevek = (ArrayList<String>) nevek.clone();
+        Collator hu = Collator.getInstance(Locale.getDefault());
+
+        Collections.sort(ujNevek,hu);
+       /* System.out.println(ujNevek);
+        System.out.println("---------------------");
+        System.out.println(nevek); */
+        for (String n:ujNevek) {
+            System.out.print(n+" ");
+        }
+        System.out.println("----------------------");
+        for (int i = 0; i < ujNevek.size(); i++) {
+            System.out.print(ujNevek.get(i)+" ");
+        }
+        System.out.println();
     }
 
     private static void beolvas() throws FileNotFoundException {
